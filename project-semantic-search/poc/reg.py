980b9f416b6b88a03033eca04801dbbc58c00509
ram krishnan <ramkri123@gmail.com>
2024-06-09 14:36:16 -0700

    regex

--- project-semantic-search/poc/reg.py
+++ project-semantic-search/poc/reg.py
@@ -1,5 +1,6 @@
 import re
 
+'''
 # Define the string to be searched
 string = "Hello, World!, Hello"
 
@@ -32,3 +33,26 @@ for i in range(len(string)):
        new_string1 += string[i]
 
 print(new_string1)
+
+s = 'one two one two one'
+print(s)
+
+print(s.replace('one', 'XXX'))
+# XXX two XXX two XXX
+
+print(s.replace('one', 'XXX', 2))
+# XXX two XXX two one
+'''
+
+special_char_dict = {
+  'àtma-jñànì': 'atma-jnani',
+  'daréan': 'darsan',
+}
+text = 'When Käçåa says a friend to all beings, he doesn’t just mean all types of people. The heart of the àtma-jñànì extends with the feeling of friendship to every aspect of creation. Amma’s biography is filled with stories illustrating the reciprocal heartfelt bond between Amma and various animals, and I have personally seen Amma give daréan not just to common pets like cats and dogs, but also to parrots, eagles, horses, wolves, cows, goats, camels, beetles, owls, rabbits, chipmunks, bats, turtles, pythons, boa constrictors, monkeys, cheetahs, and elephants. Amma’s heart truly has a space for every aspect of creation. It never ceases to amaze me how she never forgets to consider the needs and feelings of even those creatures we typically think of as insignificant.'
+print("*** original text ***")
+print(text)
+for s in special_char_dict:
+    print(s, special_char_dict[s])
+    text = text.replace(s, special_char_dict[s])
+print("*** modified text ***")
+print(text)

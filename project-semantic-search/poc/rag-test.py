7da1616ee686da9425cf090be3dd910447b1f534
ram krishnan <ramkri123@gmail.com>
2024-06-08 19:07:22 -0700

    test

---
+++ project-semantic-search/poc/rag-test.py
@@ -0,0 +1,14 @@
+from transformers import RagTokenizer, RagRetriever, RagSequenceForGeneration 
+ 
+tokenizer = RagTokenizer.from_pretrained("facebook/rag-sequence-nq") 
+retriever = RagRetriever.from_pretrained("facebook/rag-sequence-nq", index_name="exact", use_dummy_dataset=True) 
+model = RagSequenceForGeneration.from_pretrained("facebook/rag-sequence-nq", retriever=retriever) 
+ 
+input_dict = tokenizer.prepare_seq2seq_batch("how many countries are in europe", return_tensors="pt") 
+
+generated = model.generate(input_ids=input_dict["input_ids"]) 
+print(input_dict)
+print(tokenizer.batch_decode(generated, skip_special_tokens=True)[0]) 
+
+# should give 54 => google says either 44 or 51
+
